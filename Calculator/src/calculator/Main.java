fbe76646d2d376a31b6c046cf6a7e176bcc9257f
duncangrubbs <duncan.grubbs@gmail.com>
2015-06-15 19:48:44 -0700

    Add main code.

---
+++ Calculator/src/calculator/Main.java
@@ -0,0 +1,14 @@
+package calculator;
+import java.util.Scanner;
+
+public class Main extends Methods{
+	
+	public static void main(String[] args){
+		System.out.println("+ = addition");
+		System.out.println("- = subtraction");
+		System.out.println("x = multiplication");
+		System.out.println("/ = division");
+		methodCaller.main();
+	}
+
+}
